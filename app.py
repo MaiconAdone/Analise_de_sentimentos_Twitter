e92d9d2ebbd1a9a47df8ac7b7f0a0cb2ec09b4de
Maicon Adone <117459220+MaiconAdone@users.noreply.github.com>
2023-02-27 10:10:52 -0300

    Arquivo da aplicação
    Arquivop para rodar aplicação web

---
+++ app.py
@@ -0,0 +1,172 @@
+
+#!pip install flask
+
+# importing required libraries
+import pandas as pd
+from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS, TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import f1_score
+from sklearn.model_selection import train_test_split
+
+# read the dataset
+data = pd.read_csv('twitter_sentiments.csv')
+# view the top rows
+data.head()
+
+# train test split
+train, test = train_test_split(data, test_size = 0.2, stratify = data['label'], random_state=21)
+
+# get the shape of train and test split.
+train.shape, test.shape
+## >> ((25569, 3), (6393, 3))
+
+# create a TF-IDF vectorizer object
+tfidf_vectorizer = TfidfVectorizer(lowercase= True, max_features=1000, stop_words=ENGLISH_STOP_WORDS)
+
+# fit the object with the training data tweets
+tfidf_vectorizer.fit(train.tweet)
+
+# transform the train and test data
+train_idf = tfidf_vectorizer.transform(train.tweet)
+test_idf  = tfidf_vectorizer.transform(test.tweet)
+
+# create the object of LinearRegression Model
+model_LR = LogisticRegression()
+
+# fit the model with the training data
+model_LR.fit(train_idf, train.label)
+
+# predict the label on the traning data
+predict_train = model_LR.predict(train_idf)
+
+# predict the model on the test data
+predict_test = model_LR.predict(test_idf)
+
+# f1 score on train data
+f1_score(y_true= train.label, y_pred= predict_train)
+## >> 0.4888178913738019
+
+f1_score(y_true= test.label, y_pred= predict_test)
+## >> 0.45751633986928114
+
+# define the stages of the pipeline
+pipeline = Pipeline(steps= [('tfidf', TfidfVectorizer(lowercase=True,
+                                                      max_features=1000,
+                                                      stop_words= ENGLISH_STOP_WORDS)),
+                            ('model', LogisticRegression())])
+
+# fit the pipeline model with the training data                            
+pipeline.fit(train.tweet, train.label)
+
+# sample tweet
+text = ["Virat Kohli, AB de Villiers set to auction their 'Green Day' kits from 2016 IPL match to raise funds"]
+
+# predict the label using the pipeline
+pipeline.predict(text)
+## >> array([0])
+
+# import joblib
+from joblib import dump
+
+# dump the pipeline model
+dump(pipeline, filename="text_classification.joblib")
+
+# import joblib
+from joblib import load
+
+# sample tweet text
+text = ["Virat Kohli, AB de Villiers set to auction their 'Green Day' kits from 2016 IPL match to raise funds"]
+
+# load the saved pipleine model
+pipeline = load("text_classification.joblib")
+
+# predict on the sample tweet text
+pipeline.predict(text)
+## >> array([0])
+
+#!pip3 install tweepy
+
+# import required libraries
+import tweepy
+import time
+import pandas as pd
+pd.set_option('display.max_colwidth', 1000)
+
+# api key
+api_key = ""
+# api secret key
+api_secret_key = ""
+# access token
+access_token = ""
+# access token secret
+access_token_secret = ""
+
+# authorize the API Key
+authentication = tweepy.OAuthHandler(api_key, api_secret_key)
+
+# authorization to user's access token and access token secret
+authentication.set_access_token(access_token, access_token_secret)
+
+# call the api
+api = tweepy.API(authentication, wait_on_rate_limit=True)
+
+def get_related_tweets(text_query):
+    # list to store tweets
+    tweets_list = []
+    # no of tweets
+    count = 50
+    try:
+        # Pulling individual tweets from query
+        for tweet in api.search(q=text_query, count=count):
+            print(tweet.text)
+            # Adding to list that contains all tweets
+            tweets_list.append({'created_at': tweet.created_at,
+                                #'tweet_id': tweet.id,
+                                'tweet_text': tweet.text})
+        return pd.DataFrame.from_dict(tweets_list)
+
+    except BaseException as e:
+        print('failed on_status,', str(e))
+        time.sleep(3)
+
+# importing the required libraries
+from flask import Flask, render_template, request, redirect, url_for
+from joblib import load
+from get_tweets import get_related_tweets
+
+# load the pipeline object
+pipeline = load("text_classification.joblib")
+
+# function to get results for a particular text query
+def requestResults(name):
+    # get the tweets text
+    tweets = get_related_tweets(name)
+    # get the prediction
+    tweets['prediction'] = pipeline.predict(tweets['tweet_text'])
+    # get the value counts of different labels predicted
+    data = str(tweets.prediction.value_counts()) + '\n\n'
+    return data + str(tweets)
+
+# start flask
+app = Flask(__name__ ,template_folder='template' )
+
+# render default webpage
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+# when the post method detect, then redirect to success function
+@app.route('/', methods=['POST', 'GET'])
+def get_data():
+    if request.method == 'POST':
+        user = request.form['search']
+        return redirect(url_for('success', name=user))
+
+# get the data for the requested query
+@app.route('/success/<name>')
+def success(name):
+     return "<xmp>" + str(requestResults(name)) + " </xmp> "
+     
+if __name__ == '__main__' :
+    app.run(debug=True)
